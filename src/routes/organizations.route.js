f8957b22d4846c2f0e342266afb7bd1010b396bb
Youssef Nabil <146959536+youssefnabil2030@users.noreply.github.com>
2026-09-21 16:22:28 -0700

    Delete src directory

--- src/routes/organizations.route.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getOrganizationsPage, getOrganizationDetails } from '../controllers/organizations.controller.js';
-
-const router = Router();
-
-router.get('/organizations', getOrganizationsPage);
-router.get('/organization/:id', getOrganizationDetails);
-
-export default router;
--- src/routes/projects.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getProjectsPage, getProjectDetails } from '../controllers/projects.controller.js';
-
-const router = Router();
-
-router.get('/', getProjectsPage);
-router.get('/project/:id', getProjectDetails);
-
-export default router;
